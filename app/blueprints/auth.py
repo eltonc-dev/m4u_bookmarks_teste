2488c2a10759f93a15461e278c5f8855f511a43c
eltonc.dev <eltonc.dev@gmail.com>
2017-10-14 00:21:27 -0300

    signUp; signIn; SignOut prontos, sem layout;

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from flask import Flask , request, session , render_template , Blueprint, redirect , url_for
 
 auth_blueprint = Blueprint('auth', __name__)
@@ -7,30 +8,57 @@ auth_blueprint = Blueprint('auth', __name__)
 def index():
     if session.get('logged_user') is None:
         return render_template('sign.html')
-    return render_template('home.html')
 
-@auth_blueprint.route("/sign/in", methods=["POST"])
+    user = json.loads(session['logged_user'])
+        
+    return render_template('home.html' , user=user )
+
+@auth_blueprint.route("/sign/in", methods=["GET", "POST"])
 def signIn():
-    #return render_template('sign.html', signInError="Erro")
-    return redirect(url_for('.index', signInError="Erro"))
+    if request.method == "GET":
+        return redirect(url_for('.index'))
 
-@auth_blueprint.route("/sign/up", methods=["POST"])
+    info = request.form
+    user = {
+        'email':info['user-email'],
+        'password':info['user-password']
+    }
+
+    res = requests.post('http://api:3000/api/sign/in', data=user)
+    if res.status_code == 200:
+        loggedUser = res.json()
+        if loggedUser['token'] is not None:
+            session['logged_user'] = res.text
+            return redirect(url_for('.index'))
+    else:
+        info = res.json()['message']
+    
+    return render_template('sign.html', signInError=info)
+
+@auth_blueprint.route("/sign/up", methods=["GET", "POST"])
 def signUp():
+    if request.method == "GET":
+        return redirect(url_for('.index'))  
+
     info = request.form
     user = {
         'name':info['user-name'],
         'email':info['user-email'],
         'password':info['user-password']
     }
-    print(user)
     res = requests.post('http://api:3000/api/sign/up', data=user)
-    print(res.json())
-    
     if res.status_code == 201:
         loggedUser = res.json()
-        print(res.content.token)
-        if loggedUser.token is not None:
-            session['logged_user'] = loggedUser
+        if loggedUser['token'] is not None:
+            session['logged_user'] = res.text
             return redirect(url_for('.index'))
+    else:
+        info = res.json()['message']
 
-    return render_template('sign.html', error=info)
+    return render_template('sign.html', signUpError=info)
+
+
+@auth_blueprint.route("/sign/out", methods=["GET", "POST"])
+def signOut():
+    session.clear()
+    return redirect(url_for('.index'))
--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask , render_template, Session
 from .blueprints.auth import auth_blueprint
 
 app = Flask(__name__)
+sess = Session()
+app.secret_key = 'emequatrou'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 app.register_blueprint(auth_blueprint)
  
 
 if __name__ == "__main__":
+    
+    sess.init_app(app)
+
     # Only for debugging while developing
     app.run(host='0.0.0.0', debug=True, port=80)
